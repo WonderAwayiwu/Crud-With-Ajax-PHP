ae3fcde630cbeb3585deee85aa77af7677aac524
WonderAwayiwu <wonderawayiwu19@gmail.com>
2025-05-08 16:02:46 +0000

    removed js code for index num generation

--- student.php
+++ student.php
@@ -204,28 +204,7 @@
   <script src="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/alertify.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js" integrity="sha512-AA1Bzp5Q0K1KanKKmvN/4d3IRKVlv9PYgwFPvm32nPO6QS8yH1HO7LbgB1pgiOxPtfeg5zEn2ba64MUcqJx6CA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-  <!-- <script>
-    let currentIndex = 223344510;
-
-    function generateStudentIndex() {
-      currentIndex++;
-      const formattedIndex = String(currentIndex).padStart(10, '0');
-      return formattedIndex;
-    }
-
-    function addNewStudent() {
-      const newStudentIndex = generateStudentIndex();
-      return newStudentIndex;
-    }
-
-   
-    $(document).on('click', '.addNewStudent', function() {
-      const newIndex = addNewStudent(); 
-      $('input[name="IndexNumber"]').val(newIndex);
-    }); 
-
-  </script> -->
-
+  
 
  
 
